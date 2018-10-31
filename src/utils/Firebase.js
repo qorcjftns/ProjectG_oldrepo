73ab4ed070a832afe214747720610fc5ca116f70
qorcjftns <qorcjftns2@gmail.com>
2018-10-31 06:01:08 +0000

    firebase api key removed from git

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -2,14 +2,7 @@ import * as firebase from 'firebase';
 
 let database;
 
-var config = {
-	apiKey: "AIzaSyCJDEkg6zNBi7K2Z2B9y_JJL5iPbYbo2as",
-	authDomain: "projectg-b9024.firebaseapp.com",
-	databaseURL: "https://projectg-b9024.firebaseio.com",
-	projectId: "projectg-b9024",
-	storageBucket: "projectg-b9024.appspot.com",
-	messagingSenderId: "810147146204"
-};
+import config from './Firebase-config';
 
 export const fire = () => {
 	if (!firebase.apps.length) {
